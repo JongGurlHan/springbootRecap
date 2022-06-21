18525b74744c64e0b5f18d7157d3f3869a8be97c
JG HAN <gojkhan13@gmail.com>
2022-06-21 18:07:37 +0900

    6/21 지도 api세팅

---
+++ src/main/java/com/jghan/springbootRecap/web/HomeController.java
@@ -0,0 +1,15 @@
+package com.jghan.springbootRecap.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class HomeController {
+
+    @GetMapping({"/"})
+    public String home(){
+        return "home";
+    }
+}
